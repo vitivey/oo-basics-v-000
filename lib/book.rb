d5ebd6f7d4dff4410495354fd0ab8e66a7314f72
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-12 19:01:53 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -5,6 +5,6 @@ attr_accessor :title, :author, :page_count, :genre
   end
 
   def turn_page
-    puts "Flipping the page..wow, you read fast!"
+    puts "Flipping the page...wow, you read fast!"
   end
 end
