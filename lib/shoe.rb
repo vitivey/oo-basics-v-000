8df7101642854f671a1904c1b6065b61e5a5e5d7
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-12 19:04:07 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
 # Make your shoe class here!
+class shoe
+attr_reader :brand
+attr_accessor :color, :size, :material, :condition
+
+def initialize(brand)
+  @brand=brand
+end
+
+def cobble
+end
+
+end
